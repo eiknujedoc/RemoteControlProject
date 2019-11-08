12c01908830e333f6729705b13075d7220857134
eiknujedoc <code.junkie@comcast.net>
2019-11-08 16:53:06 -0700

    setup scope of methods main() and getButtonPressed()

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
+		
+		boolean turnedOn = false;
 		int buttonPressed = getButtonPressed();
 		
 	}
 
-	public static int getButtonPressed(buttonPressed) {
+	public static int getButtonPressed() {
 		Scanner kb = new Scanner(System.in);
+		int buttonPressed = kb.nextInt();
 		
-		return buttonPressed;
-		
-	}
+	return buttonPressed;
+	
+	}	
 }
+

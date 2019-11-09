72c1418be0565d080704ad921b3472a6c4989b3d
eiknujedoc <code.junkie@comcast.net>
2019-11-08 18:47:49 -0700

    finished

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,32 +1,3 @@
-
-/*
-User Story #1
-When the user starts the program, it will print an initial message asking for input,
-
-To begin, turn on the remote control:
-and read the user's input.
-
-User Story #2
-If the user inputs anything other than ON, the initial message repeats until ON is entered.
-
-User Story #3
-Once the user types ON, output the message "TURNING ON...". (This output should be in a separate method.)
-
-Write a method named pressButton() that prints the message "BOOP!" to the screen.
-
-The program will now ask the user to enter the button number to press. Accept the input.
-
-If the user enters a number 0 through 9, call the pressButton() method, and prompt for input again.
-
-User Story #4
-While the remote control is on, if the user enters anything other than 0 - 9 or OFF, 
-output the message "Command not recognized" and prompt for input again.
-
-User Story #5
-If the user enters OFF, output the message "TURNING OFF..." 
-(again, separate method), and the program ends.
-*/
-
 import java.util.Scanner;
 
 public class RemoteControlApp {
